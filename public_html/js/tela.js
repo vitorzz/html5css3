d9e01e47117a55f65240804795409f12fbdca5e6
Vitor Souza Zelli <vitorz00124@gmail.com>
2018-10-22 17:24:24 -0300

    adicionado

---
+++ public_html/js/tela.js
@@ -0,0 +1,3 @@
+var largura = (window.innerWidth > 0) ? window.innerWidth:screen.width;
+var altura = (window.innerHeight > 0) ? window.innerWidth:screen.height;
+document.write(largura + " x " + altura);
